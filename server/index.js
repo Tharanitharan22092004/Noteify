52e65c0de983b9f7729f7e3e261285aa25bd6e2f
Tharanitharan22092004 <tharanitharan.n2022cse@sece.ac.in>
2024-12-01 16:46:44 +0530

    Adding user Routes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import authRouter from "./routes/auth.js";
 
-const app =express();
+const app = express();
 
-app.listen(5000,()=>{
-    console.log('Server is running on port 5000');
-})
+
+app.use(cors());
+app.use(express.json()); 
+
+
+app.use("/api/auth", authRouter);
+
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
---
+++ server/routes/auth.js
@@ -0,0 +1,30 @@
+import express from "express";
+import User from "../models/User.js";  
+import bcrypt from "bcrypt";
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res
+        .status(401)
+        .json({ success: false, msg: "User already exists" });
+    }
+    const hashpassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      name,
+      email,
+      password: hashpassword,
+    });
+    await newUser.save();
+    return res.status(200).json({ success: true, msg: "Account created" });
+  } catch (err) {
+    return res.status(500).json({ success: false, msg: "Error adding user" });
+  }
+});
+
+export default router;
