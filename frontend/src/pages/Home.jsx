fd1d9e1b119b95c7823f6d5cf15657654ce6df79
Tharanitharan22092004 <tharanitharan.n2022cse@sece.ac.in>
2024-12-05 00:08:58 +0530

    Adding NoteCard

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,24 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import NoteModel from "../components/NoteModel";
-import axios from 'axios';
+import NoteCard from "../components/NoteCard"; 
+import axios from "axios";
+
 const Home = () => {
   const [isModelOpen, setModelOpen] = useState(false);
   const [notes, setNotes] = useState([]);
 
-
   useEffect(() => {
-    const fetchNotes = async () => {
-      try {
-        const response = await axios.get(
-          "http://localhost:5000/api/note");
-        setNotes(response.data.notes);
-      } catch (error) {
-        console.error(error);
-      }
-    }
     fetchNotes();
-  },[]);
+  }, []);
+  
+  const fetchNotes = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/api/note");
+      setNotes(response.data.notes);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
   const closeModel = () => {
     setModelOpen(false);
   };
@@ -36,25 +39,26 @@ const Home = () => {
         }
       );
       if (response.data.success) {
+        fetchNotes();
         closeModel();
       }
     } catch (error) {
       console.error(error);
     }
   };
+
   return (
     <div className="bg-gray-100 min-h-screen">
       <Navbar />
 
-      <div>
-        {notes.map(note =>{
-          <NoteCard
-          note={note}
-          />
-        })}:
+      <div className="px-8 pt-4 grid grid-cols-1 md:grid-cols-3 gap-5">
+        {notes.map((note) => (
+          <NoteCard key={note._id} note={note} /> 
+        ))}
       </div>
+      
       <button
-        className="fixed right-4 bottom-4 text=2xl bg-teal-500 text-white font-bold p-4 rounded-full"
+        className="fixed right-4 bottom-4 text-2xl bg-teal-500 text-white font-bold p-4 rounded-full"
         onClick={() => setModelOpen(true)}
       >
         +
